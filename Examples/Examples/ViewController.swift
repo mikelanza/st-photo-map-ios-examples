bc221eae275845ea6bd152f43709b6726e0c6559
Crasneanu Cristian <crasneanu.cristian@gmail.com>
2019-04-16 13:59:53 +0300

    - Change target and integrate STPhotoMap

--- Examples/Examples/ViewController.swift
+++ Examples/Examples/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import STPhotoMap
 
 class ViewController: UIViewController {
-
+    
+    var photoMapView: STPhotoMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.setupSubviews()
+        self.setupSubviewsConstraints()
     }
-
-
 }
+
+// MARK: - Subviews configuration
+
+extension ViewController {
+    private func setupSubviews() {
+        self.setupPhotoMapView()
+    }
+    
+    private func setupPhotoMapView() {
+        let photoMapView = STPhotoMapView()
+        let model = STPhotoTileOverlay.Model(url: "https://tilesdev.streetography.com/tile/%d/%d/%d.jpeg")
+        photoMapView.addTileOverlay(model: model)
+        photoMapView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(photoMapView)
+        self.photoMapView = photoMapView
+    }
+}
+
+// MARK: - Constraints configuration
+
+extension ViewController {
+    private func setupSubviewsConstraints() {
+        self.setupPhotoMapViewConstraints()
+    }
+    
+    private func setupPhotoMapViewConstraints() {
+        self.photoMapView?.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        self.photoMapView?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        self.photoMapView?.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        self.photoMapView?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+    }
+}
+
 
